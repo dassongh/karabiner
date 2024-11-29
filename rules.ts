568573c7fe8a3cdeec47e9549359cb42cda0c880
Alexander S <oleksandr.slv@gmail.com>
2024-11-29 22:43:48 +0200

    change hyper key and update keymaps

--- rules.ts
+++ rules.ts
@@ -1,6 +1,6 @@
 import fs from "fs";
 import { KarabinerRules } from "./types";
-import { app, createAltSubLayers, createHyperSubLayers, open } from "./utils";
+import { app, createHyperSubLayers, open } from "./utils";
 
 const rules: KarabinerRules[] = [
   // Define the Hyper key
@@ -19,48 +19,80 @@ const rules: KarabinerRules[] = [
         type: "basic",
       },
       {
-        description: "Control -> Hyper Key",
-        from: { key_code: "left_control", modifiers: { optional: ["any"] } },
+        description: "Tab -> Hyper Key",
+        from: { key_code: "tab", modifiers: { optional: ["any"] } },
         to: [{ set_variable: { name: "hyper", value: 1 } }],
+        to_if_alone: [{ key_code: "tab" }],
         to_after_key_up: [{ set_variable: { name: "hyper", value: 0 } }],
         type: "basic",
       },
     ],
   },
-  ...createAltSubLayers({
-    1: app("Calendar"),
-    2: app("Telegram"),
-    3: app("Spotify"),
-    // z is reserved for main desktop
-    x: app("WezTerm"),
-    c: app("Visual Studio Code"),
-    v: app("Arc"),
-    b: app("Postman"),
-    s: app("Slack"),
-    d: app("DBeaver"),
-    f: app("Finder"),
-    r: app("Reminders"),
+  // ...createAltSubLayers({
+  //   1: app("Calendar"),
+  //   2: app("Telegram"),
+  //   3: app("Spotify"),
+  //   // z is reserved for main desktop
+  //   x: app("WezTerm"),
+  //   c: app("Visual Studio Code"),
+  //   v: app("Arc"),
+  //   b: app("Postman"),
+  //   s: app("Slack"),
+  //   d: app("DBeaver"),
+  //   f: app("Finder"),
+  //   r: app("Reminders"),
+  //   t: app("Toggl Track"),
+  //   o: app("Obsidian"),
+  //   n: app("Notes"),
+  //   m: app("Mail"),
+  // }),
+  ...createHyperSubLayers({
+    // number row
+    1: app("WezTerm"),
+    2: app("Arc"),
+    3: app("Visual Studio Code"),
+    4: app("DBeaver"),
+    5: app("Postman"),
+    6: app("Spotify"),
+
+    // top row
+    r: app("Telegram"),
     t: app("Toggl Track"),
     o: app("Obsidian"),
+
+    // center row
+    s: app("Slack"),
+
+    // bottom row
+    c: app("Calendar"),
+    b: app("Reminders"),
     n: app("Notes"),
     m: app("Mail"),
-  }),
-  ...createHyperSubLayers({
-    // 1: app("Telegram"),
-    // 2: app("Calendar"),
-    // 3: app("Spotify"),
-    // x: app("WezTerm"),
-    // c: app("Visual Studio Code"),
-    // v: app("Arc"),
-    // a: app("Postman"),
-    // s: app("Slack"),
-    // d: app("DBeaver"),
-    // f: app("Finder"),
-    // t: app("Toggl Track"),
-    // o: app("Obsidian"),
-    // n: app("Notes"),
-    // r: app("Reminders"),
-    // m: app("Mail"),
+
+    // // Open apps
+    // e: {
+    //   // center row
+    //   // f: app("Finder"),
+    //   h: app("WezTerm"),
+    //   j: app("Visual Studio Code"),
+    //   k: app("Arc"),
+    //   l: app("DBeaver"),
+
+    //   // up row
+    //   w: app("Spotify"),
+
+    //   u: app("Slack"),
+    //   i: app("Telegram"),
+    //   e: app("Toggl Track"),
+    //   o: app("Obsidian"),
+    //   p: app("Postman"),
+
+    //   // bottom row
+    //   b: app("Reminders"),
+    //   n: app("Notes"),
+    //   m: app("Mail"),
+    //   comma: app("Calendar"),
+    // },
 
     // Vim movement
     h: { to: [{ key_code: "left_arrow" }] },
@@ -78,7 +110,7 @@ const rules: KarabinerRules[] = [
     },
 
     // Raycast
-    quote: {
+    q: {
       c: open("raycast://extensions/thomas/color-picker/pick-color"),
       e: open(
         "raycast://extensions/raycast/emoji-symbols/search-emoji-symbols"
